8ecaa9aae673fa1ddb4cebfb02e9536be6cc0676
Maxim Malyshev <malyshev21@gmail.com>
2021-02-24 16:38:25 +0300

    Adding array

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-let num;
+let num = '0';
 let score;
 
 const question1 = {
-	question1: 'вопрос1'
+	question: 'вопрос1',
+	answer: 'ответ1'
 }
 
 const question2 = {
-	question2: 'вопрос2'
+	question: 'вопрос2',
+	answer: 'ответ2'
 }
 
 const question3 = {
-	question3: 'вопрос3'
+	question: 'вопрос3',
+	answer: 'ответ3'
 }
 
 const question4 = {
-	question4: 'вопрос4'
+	question: 'вопрос4',
+	answer: 'ответ4'
 }
+
+let questionsArray = [question1, question2, question3, question4]
+
+console.log(questionsArray)
