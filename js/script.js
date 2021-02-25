9fb04328136438e3c39c1fe6b171544974a07115
Maxim Malyshev <malyshev21@gmail.com>
2021-02-25 11:14:54 +0300

    Adding function check answer

--- js/script.js
+++ js/script.js
@@ -2,25 +2,42 @@ let num = '0';
 let score;
 
 const question1 = {
-	question: 'вопрос1',
-	answer: 'ответ1'
+  question: 'вопрос1',
+  correctAnswer: 'ответ1'
 }
 
 const question2 = {
-	question: 'вопрос2',
-	answer: 'ответ2'
+  question: 'вопрос2',
+  correctAnswer: 'ответ2'
 }
 
 const question3 = {
-	question: 'вопрос3',
-	answer: 'ответ3'
+  question: 'вопрос3',
+  correctAnswer: 'ответ3'
 }
 
 const question4 = {
-	question: 'вопрос4',
-	answer: 'ответ4'
+  question: 'вопрос4',
+  correctAnswer: 'ответ4'
 }
 
 let questionsArray = [question1, question2, question3, question4]
 
-console.log(questionsArray)
+let answer;
+
+function showQuestion(obj) {
+  answer = prompt(obj.question);
+  return answer;
+}
+
+showQuestion(question2)
+
+function checkAnswer(obj) {
+  if (answer === obj.correctAnswer) {
+    alert('Верно');
+  } else {
+    alert('Неверно');
+  }
+}
+
+checkAnswer(question2)
